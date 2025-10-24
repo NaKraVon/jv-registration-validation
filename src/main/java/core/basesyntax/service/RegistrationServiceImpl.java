b134d7c1baefad4837cc4ad69cf7a3250f3c453b
mr-Edoc <mr.edoc.work@gmail.com>
2025-10-24 21:37:08 +0300

    Ver4/Fixed checkstyle issues

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,9 +5,9 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
-    private final static int MIN_LENGTH = 6;
-    private final static int MIN_AGE = 18;
 
     @Override
     public User register(User user) {
